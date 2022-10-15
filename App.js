930cef55be3ddea930f39cbc929a8946575d3c8a
israell <israelpina047@gmail.com>
2022-10-15 19:02:39 -0300

    projectbugs2

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View } from 'react-native';
 import Contador from './components/Contador/Index';
 import Imagem from './components/Imagem/Index';
 import Texto from './components/Texto/index';
